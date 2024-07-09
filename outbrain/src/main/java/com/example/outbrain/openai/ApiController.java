404101556e5802ed86ede8961eaabde50e4f02ba
marijacetkovic <marijacetkovic03@gmail.com>
2024-07-09 16:26:33 +0200

    added wiki search by multiple titles

--- outbrain/src/main/java/com/example/outbrain/openai/ApiController.java
+++ outbrain/src/main/java/com/example/outbrain/openai/ApiController.java
@@ -5,8 +5,11 @@ import com.example.outbrain.openai.client.dto.PromptData;
 import com.example.outbrain.openai.client.dto.ResourceData;
 import com.example.outbrain.pdf.PDFService;
 import com.example.outbrain.wikipedia.WikipediaService;
+import com.example.outbrain.wikipedia.dto.WikipediaData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 public class ApiController {
     private final WikipediaService wikipediaService;
@@ -26,13 +29,16 @@ public class ApiController {
     }
 
     @GetMapping("/resourcefinder")
-    public void findResources(@PathVariable String filePath, @PathVariable String prompt){
+    public void findResources(@PathVariable String filePath, @PathVariable String prompt, @PathVariable int limit){
+        //convert pdf
         String document = pdfService.convertPDF(filePath);
         PromptData data = new PromptData(prompt, document);
+
+        //get keywords
         ResourceData keywords = gptApiClient.getResourceData(data);
+
         //search wiki for keywords
-
-
+        wikipediaService.findByMultipleTitle(keywords.keywords(),limit);
     }
 
 }
