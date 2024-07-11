83b6940559b4cc8eab661f7af781ee02ebd15e45
marijacetkovic <marijacetkovic03@gmail.com>
2024-07-11 11:25:56 +0200

    frontend boilerplate

---
+++ front-end/src/app/views/about/about.view.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './about.view.html',
+    styleUrls: ['./about.view.less'],
+})
+export class AboutView {
+
+}
---
+++ front-end/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    backendBaseUrl: 'https://backend.greenisland-0015b907.westeurope.azurecontainerapps.io'
+};
